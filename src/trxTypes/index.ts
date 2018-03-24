ba03be760d122cb34041cca4d487f9d19f898cb9
Andrea Baccega <vekexasia@gmail.com>
2018-03-24 10:52:48 +0100

    removed now deprecated transaction types.

--- src/trxTypes/index.ts
+++ src/trxTypes/index.ts
@@ -1,22 +1,15 @@
 import { BaseTx } from './BaseTx';
 import { CreateSignatureTx, ICreateSignatureAssetType} from './CreateSignature';
-import { DappTx , IDapp, IDappAsset } from './Dapp';
 import { DelegateTx, IDelegateTxAsset } from './Delegate';
-import { IInTransferAsset, InTransferTx } from './InTransfer';
 import { IMultiSignatureAsset, MultiSignatureTx } from './MultiSignature';
-import { IOutTransferAsset, OutTransferTx } from './OutTransfer';
 import { SendTx } from './Send';
 import { IVoteAsset, VoteTx } from './Vote';
 
 // Exporting asset types
 export {
   ICreateSignatureAssetType,
-  IDappAsset,
-  IDapp,
   IDelegateTxAsset,
-  IInTransferAsset,
   IMultiSignatureAsset,
-  IOutTransferAsset,
   IVoteAsset,
 };
 
@@ -24,11 +17,8 @@ export {
 export {
   BaseTx,
   CreateSignatureTx,
-  DappTx,
   DelegateTx,
-  InTransferTx,
   MultiSignatureTx,
-  OutTransferTx,
   SendTx,
   VoteTx,
 };
